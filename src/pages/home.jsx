134be1d5832b0eebcf4cb528a25c72076f090e0a
jhayashi1 <jhayashi@iastate.edu>
2022-08-28 17:33:16 -0500

    homepage formatting

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,8 +8,25 @@ class Home extends React.Component {
                 <div className='home-banner'>
                     <h1 className='home-title'>Jared Hayashi</h1>
                     <h2 className='home-description'>
-                        Currently a senior at Iowa State University graduating in the Fall of 2022
+                        Software Engineering Student @ Iowa State University
                     </h2>
+                </div>
+
+                <hr width="75%" />
+
+                <div className='about'>
+                    <div className='about-text'>
+                        <h3 className='about-title'>About:</h3>
+                        <div className='about-description-div'>
+                            <h4 className='about-description'>
+                            Hi my name is Jared, and I am currently a senior at Iowa State University. I will be graduating in Fall of 2022
+                            with a bachelor's degree in Software Engineering.
+                            </h4>
+                            <h4 className='about-description'>
+                                More here
+                            </h4>
+                        </div>
+                    </div>
                 </div>
 
                 <hr width="75%" />
@@ -19,13 +36,16 @@ class Home extends React.Component {
 
                     {renderProjectContent(
                         'Gnutella Network',
-                        `A proof of concept file sharing network based on the Gnutella protocol.`,
+                        `A proof of concept file sharing network based on the Gnutella protocol. \n
+                        
+                        Learn more here https://en.wikipedia.org/wiki/Gnutella
+                        `,
                         '/gnutella'
                     )}
 
                     {renderProjectContent(
-                        'Epic 7 Auto Shop',
-                        `A that uses image recognition to automate buying items from a shop in a game called Epic Seven.
+                        'Epic Seven Auto Shop',
+                        `A tool that uses image recognition to automate buying items from a shop in a game called Epic Seven.
                         
                         `,
                         '/e7-auto-shop'
@@ -68,7 +88,7 @@ function renderProjectContent(title, description, link) {
         <>
             <a className='project-title' href={link}>{title}</a>
             <h4 className='project-description'>{description}</h4>
-            <hr width="75%" />
+            <hr width="80%" />
         </>
     );
 }
